d7356b4564aead7df8c5b36913c133087059df5a
HusnaMa <husna@htsqatar.com>
2024-05-17 06:56:05 +0300

    payment means version update

--- zatca2024/zatca2024/createxml.py
+++ zatca2024/zatca2024/createxml.py
@@ -412,7 +412,7 @@ def delivery_And_PaymentMeans(invoice,sales_invoice_doc, is_return):
                 cbc_ActualDeliveryDate.text = str(sales_invoice_doc.due_date)
                 cac_PaymentMeans = ET.SubElement(invoice, "cac:PaymentMeans")
                 cbc_PaymentMeansCode = ET.SubElement(cac_PaymentMeans, "cbc:PaymentMeansCode")
-                cbc_PaymentMeansCode.text = "32"
+                cbc_PaymentMeansCode.text = "30"
                 
                 if is_return == 1:
                     cbc_InstructionNote = ET.SubElement(cac_PaymentMeans, "cbc:InstructionNote")
@@ -427,7 +427,7 @@ def delivery_And_PaymentMeans_for_Compliance(invoice,sales_invoice_doc, complian
                 cbc_ActualDeliveryDate.text = str(sales_invoice_doc.due_date)
                 cac_PaymentMeans = ET.SubElement(invoice, "cac:PaymentMeans")
                 cbc_PaymentMeansCode = ET.SubElement(cac_PaymentMeans, "cbc:PaymentMeansCode")
-                cbc_PaymentMeansCode.text = "32"
+                cbc_PaymentMeansCode.text = "30"
                 
                 if compliance_type == "3" or compliance_type == "4" or compliance_type == "5" or compliance_type == "6":
                     cbc_InstructionNote = ET.SubElement(cac_PaymentMeans, "cbc:InstructionNote")
